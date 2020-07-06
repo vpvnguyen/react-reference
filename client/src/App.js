07701748641ff3ea3b9a36959b9958ac43196048
Vincent Nguyen <vpvnguyen@gmail.com>
2020-07-05 20:27:47 -0700

    completed reach router basic, url params, nested routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import ReachIndex from "./components/Reach";
+
 function App() {
-  return <div className="App">Heyo</div>;
+  return (
+    <div className="App">
+      <ReachIndex />
+    </div>
+  );
 }
 
 export default App;
